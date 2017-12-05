e6c69feb3c9daeeee774677ba021f3852498b350
Slava Vikharev <slavavikharev@yandex-team.ru>
2017-12-06 01:02:21 +0500

    Задача «Пора на лекцию»

--- emitter.js
+++ emitter.js
@@ -1,68 +1,50 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализованы методы several и through
- */
-getEmitter.isStar = true;
-module.exports = getEmitter;
+let store = [];
 
-/**
- * Возвращает новый emitter
- * @returns {Object}
- */
-function getEmitter() {
-    return {
+module.exports = () => ({
+    isStar: false,
 
-        /**
-         * Подписаться на событие
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
-        },
+    on: function (event, context, handler) {
+        store.push([event.split('.'), context, handler]);
 
-        /**
-         * Отписаться от события
-         * @param {String} event
-         * @param {Object} context
-         */
-        off: function (event, context) {
-            console.info(event, context);
-        },
+        return this;
+    },
 
-        /**
-         * Уведомить о событии
-         * @param {String} event
-         */
-        emit: function (event) {
-            console.info(event);
-        },
+    off: function (event, context) {
+        let eventSplitted = event.split('.');
 
-        /**
-         * Подписаться на событие с ограничением по количеству полученных уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} times – сколько раз получить уведомление
-         */
-        several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
-        },
+        store = store.filter(
+            ([storeEvent, storeContext]) => {
+                let slicedStoreEvent = storeEvent.slice(0, eventSplitted.length);
 
-        /**
-         * Подписаться на событие с ограничением по частоте получения уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
-         */
-        through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
-        }
-    };
-}
+                return storeContext === context &&
+                    eventSplitted.join('.') === slicedStoreEvent.join('.');
+            }
+        );
+
+        return this;
+    },
+
+    emit: function (event) {
+        let eventSplitted = event.split('.');
+
+        store.forEach(
+            ([storeEvent, storeContext, storeHandler]) => {
+                let minEventNamesLength = Math.min(
+                    eventSplitted.length,
+                    storeEvent.length
+                );
+
+                let slicedEvent = eventSplitted.slice(0, minEventNamesLength);
+                let slicedStoreEvent = storeEvent.slice(0, minEventNamesLength);
+
+                if (slicedEvent.join('.') === slicedStoreEvent.join('.')) {
+                    storeHandler.call(storeContext);
+                }
+            }
+        );
+
+        return this;
+    }
+});
